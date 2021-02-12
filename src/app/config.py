eda2998027a1c3d7c1ce220760eefd4687fd842f
azat.zakirov <zakirmalay@gmail.com>
2021-02-13 02:51:47 +0500

    fix(CORS): исправлена ошибка localhost regex

--- src/app/config.py
+++ src/app/config.py
@@ -6,4 +6,4 @@ TIMEOUT = 5  # seconds
 
 CORS_DOMAINS = env.get('CORS_DOMAINS')
 CORS_DOMAINS = CORS_DOMAINS.split(',') if CORS_DOMAINS else []
-CORS_DOMAINS += [r'http[s]?://localhost:[0-9]{4}']
+CORS_DOMAINS += [r'http[s]?:\/\/localhost[:]?[0-9]*']
